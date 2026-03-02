26dc15e932ccb4b519cedc7f6f2504cba43444a2
Andrew Snodgrass <pointlander@gmail.com>
2026-03-02 14:40:07 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/pointlander/nbody
+
+go 1.25.0
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pointlander/gradient v0.0.0-20260301191349-1d3df6f5ab0d // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,143 @@
+// Copyright 2026 The nbody Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"strings"
+
+	"github.com/pointlander/gradient/tf64"
+)
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = 1.0e-2
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
+)
+
+// Fisher is the fisher iris data
+type Fisher struct {
+	Measures  []float64
+	Embedding []float64
+}
+
+// LearnEmbedding learns the embeddings
+func LearnEmbedding(iris []Fisher, size, width, iterations int) []Fisher {
+	rng := rand.New(rand.NewSource(1))
+	others := tf64.NewSet()
+	length := len(iris)
+	cp := make([]Fisher, length)
+	copy(cp, iris)
+	others.Add("x", size, len(cp))
+	x := others.ByName["x"]
+	for _, row := range iris {
+		x.X = append(x.X, row.Measures...)
+	}
+
+	set := tf64.NewSet()
+	set.Add("i", width, len(cp))
+
+	for ii := range set.Weights {
+		w := set.Weights[ii]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float64, StateTotal)
+			for ii := range w.States {
+				w.States[ii] = make([]float64, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for range cap(w.X) {
+			w.X = append(w.X, rng.NormFloat64()*factor*.01)
+		}
+		w.States = make([][]float64, StateTotal)
+		for ii := range w.States {
+			w.States[ii] = make([]float64, len(w.X))
+		}
+	}
+
+	drop := .3
+	dropout := map[string]interface{}{
+		"rng":  rng,
+		"drop": &drop,
+	}
+
+	sa := tf64.T(tf64.Mul(tf64.Dropout(tf64.Square(set.Get("i")), dropout), tf64.T(others.Get("x"))))
+	loss := tf64.Avg(tf64.Quadratic(others.Get("x"), sa))
+
+	for iteration := range iterations {
+		pow := func(x float64) float64 {
+			y := math.Pow(x, float64(iteration+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return y
+		}
+
+		set.Zero()
+		others.Zero()
+		l := tf64.Gradient(loss).X[0]
+		if math.IsNaN(float64(l)) || math.IsInf(float64(l), 0) {
+			fmt.Println(iteration, l)
+			return nil
+		}
+
+		norm := 0.0
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = math.Sqrt(norm)
+		b1, b2 := pow(B1), pow(B2)
+		scaling := 1.0
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range set.Weights {
+			for ii, d := range w.D {
+				g := d * scaling
+				m := B1*w.States[StateM][ii] + (1-B1)*g
+				v := B2*w.States[StateV][ii] + (1-B2)*g*g
+				w.States[StateM][ii] = m
+				w.States[StateV][ii] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				if vhat < 0 {
+					vhat = 0
+				}
+				_ = mhat
+				// w.X[ii] -= Eta * mhat / (math.Sqrt(vhat) + 1e-8)
+				if rng.Float64() > .01 {
+					w.X[ii] -= .05 * g
+				} else {
+					w.X[ii] += .05 * g
+				}
+			}
+		}
+		fmt.Println(l)
+	}
+
+	return cp
+}
+
+func main() {
+
+}
